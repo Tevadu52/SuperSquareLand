3d108d465810caec585adc4e77641d866ffb6506
Tevadu52 <tevafoug@gmail.com>
2024-04-26 12:09:17 +0200

    Cour 4, part 5

--- Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
@@ -8,15 +8,21 @@ public class CameraProfile : MonoBehaviour
     [Header("Follow")]
     [SerializeField] private CameraFollowable _targetToFollow = null;
 
+    [Header("Damping")]
+    [SerializeField] private bool _useDampingHorizontally = false;
+    [SerializeField] private float _horizontalDampingFactor = 5f;
+    [SerializeField] private bool _useDampingVertically = false;
+    [SerializeField] private float _verticalDampingFactor = 5f;
+
     private Camera _camera;
-
     public float CameraSize => _camera.orthographicSize;
-
     public Vector3 Position => _camera.transform.position;
-
     public CameraProfileType ProfileType => _cameraProfilType;
-
     public CameraFollowable TargetToFollow => _targetToFollow;
+    public bool UseDampingHorizontally => _useDampingHorizontally;
+    public float HorizontalDampingFactor => _horizontalDampingFactor;
+    public bool UseDampingVertically => _useDampingVertically;
+    public float VerticalDampingFactor => _verticalDampingFactor;
 
 
     private void Awake()
